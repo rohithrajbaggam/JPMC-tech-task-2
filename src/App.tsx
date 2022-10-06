7b73df4e4d2e3da7dd40b962f118cbdf5109cc30
rohithrajbaggam <rohithbaggam.github@gmail.com>
2022-10-06 18:09:40 +0530

    Uploading code to server

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import DataStreamer, { ServerRespond } from "./DataStreamer";
+import Graph from "./Graph";
+import "./App.css";
+
+/**
+ * State declaration for <App />
+ */
+interface IState {
+  data: ServerRespond[];
+  showGraph: boolean;
+}
+
+/**
+ * The parent element of the react app.
+ * It renders title, button and Graph react element.
+ */
+class App extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      // data saves the server responds.
+      // We use this state to parse data down to the child element (Graph) as element property
+      data: [],
+      showGraph: false,
+    };
+  }
+
+  /**
+   * Render Graph react component with state.data parse as property data
+   */
+  renderGraph() {
+    //checking if user clicked or not  
+    if (this.state.showGraph) {
+      // return the graph component 
+      return (<Graph data={this.state.data}/>)
+    }
+  }
+
+  /**
+   * Get new data from server and update the state with the new data
+   */
+  getDataFromServer() {
+    //using a counter to clear the inerval after 1000ms 
+    let x = 0;
+    //using arrow function to avoid "this" binding problem
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+       //setting the data to serverrespond data and showgraph as true
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+       //clear interval after 1000ms
+        clearInterval(interval);
+      }
+    }, 100);
+    //calling the arrow function for each 100ms 
+  }
+
+  /**
+   * Render the App react component
+   */
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Bank & Merge Co Task 2</header>
+        <div className="App-content">
+          <button
+            className="btn btn-primary Stream-button"
+            // when button is click, our react app tries to request
+            // new data from the server.
+            // As part of your task, update the getDataFromServer() function
+            // to keep requesting the data every 100ms until the app is closed
+            // or the server does not return anymore data.
+            onClick={() => {
+              this.getDataFromServer();
+            }}
+          >
+            Start Streaming Data
+          </button>
+          <div className="Graph">{this.renderGraph()}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Graph.tsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Table } from '@finos/perspective';
+import { ServerRespond } from './DataStreamer';
+import './Graph.css';
+
+/**
+ * Props declaration for <Graph />
+ */
+interface IProps {
+  data: ServerRespond[],
+}
+
+/**
+ * Perspective library adds load to HTMLElement prototype.
+ * This interface acts as a wrapper for Typescript compiler.
+ */
+ interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void,
+}
+
+/**
+ * React component that renders Perspective based on data
+ * parsed from its parent through data property.
+ */
+class Graph extends Component<IProps, {}> {
+  // Perspective table
+  table: Table | undefined;
+
+  render() {
+    return React.createElement('perspective-viewer');
+  }
+
+  componentDidMount() {
+    // Get element to attach the table from the DOM.
+    // Get element to attach the table from the DOM.
+    //no need to tell that element is  PerspectiveViewerElement because we extend the HTMLElement
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+
+    const schema = {
+      stock: 'string',
+      top_ask_price: 'float',
+      top_bid_price: 'float',
+      timestamp: 'date',
+    };
+
+    if (window.perspective && window.perspective.worker()) {
+      this.table = window.perspective.worker().table(schema);
+    }
+    if (this.table) {
+      // Load the `table` in the `` DOM reference.
+      elem.load(this.table);
+      /* adding attribute to Perspective-viewer elements*/
+      /*set view to y_line*/
+      elem.setAttribute('view','y_line');
+      /* set column-pivots to stock (ABC or DEF)*/
+      elem.setAttribute('column-pivots','["stock"]');
+       /* set row-pivots to timestamp*/
+      elem.setAttribute('row-pivots','["timestamp"]');
+     /* set columns to top_ask_price */
+      elem.setAttribute('columns','["top_ask_price"]');
+      /*aggregates to avoid duplicate data*/
+       elem.setAttribute('aggregates','{"stock":"distinct count","top_ask_price":"avg","top_bid_price": "avg","timestamp": "distinct count"}');
+    }
+  }
+
+  componentDidUpdate() {
+    // Everytime the data props is updated, insert the data into Perspective table
+    if (this.table) {
+      // As part of the task, you need to fix the way we update the data props to
+      // avoid inserting duplicated entries into Perspective table again.
+      this.table.update(this.props.data.map((el: any) => {
+        // Format the data from ServerRespond to the schema
+        return {
+          stock: el.stock,
+          top_ask_price: el.top_ask && el.top_ask.price || 0,
+          top_bid_price: el.top_bid && el.top_bid.price || 0,
+          timestamp: el.timestamp,
+        };
+      }));
+    }
+  }
+}
+
+export default Graph;
